fffcaba97fa51ec8d89930e9c808de47d3e402b6
arkkab <arkkab@gmail.com>
2020-10-19 19:58:05 +0200

    final commit, add mobile events

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const img1El = document.querySelector('.slider__image-container--first img');
     const img2El = document.querySelector('.slider__image-container--second img');
     let dragging = false;
-    const imagesContainerLeftOffset = imagesContainerEl.offsetLeft;
     const imgContainer1El = document.querySelector('.slider__image-container--first');
     const imgContainer2El = document.querySelector('.slider__image-container--second');
     const handleEl = document.querySelector('.slider__handle');
     const dividerEl = document.querySelector('.slider__divider');
     let imageContainerWidth;
+    let imagesContainerLeftOffset;
 
     function getOffset(clientX) {
         const offset = clientX - imagesContainerLeftOffset;
@@ -37,15 +37,27 @@ document.addEventListener('DOMContentLoaded', () => {
         handleEl.addEventListener('mouseup', ()=> {
             dragging = false;
         });
+        handleEl.addEventListener('touchstart', ()=> {
+            dragging = true;
+        });
+        handleEl.addEventListener('touchend', ()=> {
+            dragging = false;
+        });
         window.addEventListener('mousemove', event => {
             if(dragging) {
                 move(event.clientX);
+            }
+        });
+        window.addEventListener('touchmove', event => {
+            if(dragging) {
+                move(event.touches[0].clientX);
             }
         })
     }
 
     function adjustImagesSize() {
         imageContainerWidth = imagesContainerEl.offsetWidth;
+        imagesContainerLeftOffset = imagesContainerEl.offsetLeft;
         img1El.style.width = imageContainerWidth + 'px';
         img2El.style.width = imageContainerWidth + 'px';
     }
