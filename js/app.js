a73507d2935235abe9ea3f732bffcfc39ea03288
arkkab <arkkab@gmail.com>
2020-10-19 12:01:02 +0200

    add scss file, style for slider

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 import '../sass/style.scss';
-console.log('ok!');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imagesContainerEl = document.querySelector('.slider__images-container');
+    const img1El = document.querySelector('.slider__image-container--first img');
+    const img2El = document.querySelector('.slider__image-container--second img');
+
+    function adjustImagesSize() {
+        const imageContainerWidth = imagesContainerEl.offsetWidth;
+        img1El.style.width = imageContainerWidth + 'px';
+        img2El.style.width = imageContainerWidth + 'px';
+    }
+    window.addEventListener('resize', adjustImagesSize);
+    adjustImagesSize();
+})
